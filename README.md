d8448186b578b8512e0254594ffd7ab901d6d4a9
Panayiotis Halouvas <phalouvas@kainotomo.com>
2023-12-19 06:50:54 +0000

    Add import data and publish all items to webshop functionality

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Customizations of theodoulouparts.com
 #### License
 
 MIT
+
+## Import data
+* Import Item Groups
+* Create Supplier Group Parts
+* Import Suppliers
+* Import Item
+* Import Item Barcodes
+* Import Item Alternatives
+* When final Import Stock Entry
+
+## How to publish all items to webshop
+* ```shell
+bench console
+import importlib
+from theodoulou.theodoulou.utils.utils import publish_all_items
+from theodoulou.theodoulou.utils import utils
+importlib.reload(utils)
+utils.publish_all_items()
+``````
---
+++ theodoulou/theodoulou/utils/utils.py
@@ -0,0 +1,36 @@
+import frappe
+from webshop.webshop.doctype.website_item.website_item import make_website_item
+
+def publish_all_items():
+
+    # Delete all website items
+    #print('Deleting all website items')
+    #frappe.db.sql('DELETE FROM `tabWebsite Item`')
+    #frappe.db.commit()
+
+    #items = frappe.get_all('Item', filters={'published_in_website': 1}, fields=['name'])
+    #for i, item in enumerate(items):
+    #    print(f'Unpublishing item {item.name}')
+    #    frappe.db.set_value('Item', item.name, 'published_in_website', 0)
+        # every 100 items, commit the changes
+    #    if i % 100 == 0:
+    #        frappe.db.commit()
+    #frappe.db.commit()
+    #return;
+
+    # Get the names of all items that are not published_in_website
+    items = frappe.get_all('Item', filters={'published_in_website': 0}, fields=['name'])
+
+    # print the total number of items
+    print(f'There are {len(items)} items to publish')
+    
+    # publish each item and print the name of the item and the row number of the array
+    for i, item in enumerate(items):
+        print(f'Publishing item {item.name} at row {i}')
+        make_website_item(frappe.get_doc('Item', item.name), save=True)
+        # every 100 items, commit the changes
+        if i % 100 == 0:
+            frappe.db.commit()
+        
+    frappe.db.commit()
+    
