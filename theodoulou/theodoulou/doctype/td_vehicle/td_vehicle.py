8aa1c146c02650de442b509e36de0c10106a7b1b
Panayiotis Halouvas <phalouvas@kainotomo.com>
2024-01-13 07:35:38 +0000

    Doctype for cars Fixes #2

--- theodoulou/theodoulou/doctype/td_vehicle/td_vehicle.py
+++ theodoulou/theodoulou/doctype/td_vehicle/td_vehicle.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2024, KAINOTOMO PH LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
@@ -17,8 +17,30 @@ class TDVehicle(Document):
 		pass
 
 	@staticmethod
-	def get_list(args):
-		pass
+	def get_list(args):		
+		frappe.db.sql("SET @NEEDYEAR = '0', @SPRACHNR = 4, @LKZ = 'CY';")  # Set the variables
+		data = frappe.db.sql("""
+			SELECT distinct
+				T100.HERNR AS name,
+				GET_LBEZNR(T100.LBEZNR, @SPRACHNR) AS lbeznr,
+				T100.PKW as pkw,
+				T100.NKW as nkw,
+				T100.TRANSPORTER as transporter,
+				T100.ACHSE as achse,
+				T100.MOTOR as motor,
+				T100.GETRIEBE as getriebe
+			FROM `110` AS T110
+				JOIN `100` AS T100 ON T100.HERNR = T110.HERNR
+			WHERE 1
+				AND (CASE
+						WHEN @NEEDYEAR = 0 OR LENGTH(@NEEDYEAR) <> 4 THEN 1
+						WHEN T110.BJVON <= CAST(CONCAT(@NEEDYEAR,'01') AS UNSIGNED) AND IFNULL(T110.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT(@NEEDYEAR,'01') AS UNSIGNED) THEN 1
+						ELSE 0
+					END) = 1
+			ORDER BY NAME;
+		""", as_dict=1)
+
+		return data
 
 	@staticmethod
 	def get_count(args):
