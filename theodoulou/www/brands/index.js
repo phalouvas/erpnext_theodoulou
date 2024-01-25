f6e93416e2d5943ffd37402bc537f7ad3263aef9
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-25 07:22:22 +0000

    Add search functionality and separate brands into passenger and commercial cars

---
+++ theodoulou/www/brands/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('#passenger_cars_searchKey').on('keyup', function() {
+        var searchKey = $(this).val().toLowerCase();
+
+        $('.passenger_cars_card_container').each(function() {
+            var name = $(this).data('name');
+
+            if (name.indexOf(searchKey) !== -1) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+
+    $('#commercial_cars_searchKey').on('keyup', function() {
+        var searchKey = $(this).val().toLowerCase();
+
+        $('.commercial_cars_card_container').each(function() {
+            var name = $(this).data('name');
+
+            if (name.indexOf(searchKey) !== -1) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+});
