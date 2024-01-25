533c16a9795dc9eb2e5d7d3c716dd942fd9a84a1
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-25 13:36:23 +0000

    Add get_types_passenger_cars and get_types_commercial_cars methods to TheodoulouQuery class

--- theodoulou/theodoulou/data_engine/query.py
+++ theodoulou/theodoulou/data_engine/query.py
@@ -56,3 +56,55 @@ class TheodoulouQuery():
             """, as_dict=True)
     
             return data
+    
+    def get_types_passenger_cars(self, KMODNR, NEEDYEAR = 0):
+        if not NEEDYEAR:
+            NEEDYEAR = 0
+        data = frappe.db.sql(f"""
+            SELECT
+                T120.KTYPNR,  -- ID TYPE
+                GET_LBEZNR(T100.LBEZNR, { self.language }) AS MANUFACTURER,  -- NAME MANUFACTURER
+                GET_LBEZNR(T110.LBEZNR, { self.language }) AS MODEL,  -- NAME MODEL
+                GET_LBEZNR(T120.LBEZNR, { self.language }) AS TYPE,  -- NAME TYPE
+                T120.BJVON AS FROM_YEAR, -- TYPE PRODUCTING FROM YEAR+MONTH
+                IFNULL(T120.BJBIS, 'now') AS TO_YEAR -- TYPE PRODUCTING TO YEAR+MONTH								
+            FROM `120` AS T120
+                JOIN `110` AS T110 ON T110.KMODNR = T120.KMODNR			
+                JOIN `100` AS T100 ON T100.HERNR = T110.HERNR			
+            WHERE 1 
+                AND T110.KMODNR = { KMODNR }
+                AND (CASE
+                        WHEN {  NEEDYEAR } = 0 OR LENGTH({  NEEDYEAR }) <> 4 THEN 1					
+                        WHEN T120.BJVON <= CAST(CONCAT({  NEEDYEAR },'01') AS UNSIGNED) AND IFNULL(T120.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({  NEEDYEAR },'01') AS UNSIGNED) THEN 1
+                        ELSE 0
+                    END) = 1
+            ORDER BY T120.SORTNR;	
+        """, as_dict=True)
+
+        return data
+    
+    def get_types_commercial_cars(self, KMODNR, NEEDYEAR = 0):
+        if not NEEDYEAR:
+            NEEDYEAR = 0
+        data = frappe.db.sql(f"""
+            SELECT
+                T532.NTYPNR,  -- ID TYPE
+                GET_LBEZNR(T100.LBEZNR, { self.language }) AS MANUFACTURER,  -- NAME MANUFACTURER
+                GET_LBEZNR(T110.LBEZNR, { self.language }) AS MODEL,  -- NAME MODEL
+                GET_LBEZNR(T532.LBEZNR, { self.language }) AS TYPE,  -- NAME TYPE
+                T532.BJVON AS FROM_YEAR, -- TYPE PRODUCTING FROM YEAR+MONTH
+                IFNULL(T532.BJBIS, 'now') AS TO_YEAR -- TYPE PRODUCTING TO YEAR+MONTH								
+            FROM `532` AS T532
+                JOIN `110` AS T110 ON T110.KMODNR = T532.KMODNR			
+                JOIN `100` AS T100 ON T100.HERNR = T110.HERNR			
+            WHERE 1 
+                AND T110.KMODNR = { KMODNR }
+                AND (CASE
+                        WHEN { NEEDYEAR } = 0 OR LENGTH({ NEEDYEAR }) <> 4 THEN 1					
+                        WHEN T532.BJVON <= CAST(CONCAT({ NEEDYEAR },'01') AS UNSIGNED) AND IFNULL(T532.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({ NEEDYEAR },'01') AS UNSIGNED) THEN 1
+                        ELSE 0
+                    END) = 1
+            ORDER BY T532.SORT;		
+        """, as_dict=True)
+
+        return data
