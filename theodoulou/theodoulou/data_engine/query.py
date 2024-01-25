f1eada403879725d36073dbb7a46efaddf58c49d
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-25 07:58:53 +0000

    Add get_models function to TheodoulouQuery class

--- theodoulou/theodoulou/data_engine/query.py
+++ theodoulou/theodoulou/data_engine/query.py
@@ -26,3 +26,28 @@ class TheodoulouQuery():
         """, as_dict=True)
 
         return data
+    
+    def get_models(self, type, HERNR, NEEDYEAR = 0):
+            if not NEEDYEAR:
+                NEEDYEAR = 0
+            data = frappe.db.sql(f"""
+                SELECT
+                    T110.KMODNR,  -- ID MODEL
+                    GET_LBEZNR(T100.LBEZNR, { self.language }) AS MANUFACTURER,  -- NAME MANUFACTURER
+                    GET_LBEZNR(T110.LBEZNR, { self.language }) AS MODEL,  -- NAME MODEL
+                    T110.BJVON, -- MODEL PRODUCTING FROM YEAR+MONTH
+                    IFNULL(T110.BJBIS, 'to now') -- MODEL PRODUCTING TO YEAR+MONTH
+                FROM `110` AS T110
+                    JOIN `100` AS T100 ON T100.HERNR = T110.HERNR			
+                WHERE 1 
+                    AND T100.HERNR = { HERNR }
+                    AND T100.{ type } = 1
+                    AND (CASE
+                            WHEN { NEEDYEAR } = 0 OR LENGTH({ NEEDYEAR }) <> 4 THEN 1					
+                            WHEN T110.BJVON <= CAST(CONCAT({ NEEDYEAR },'01') AS UNSIGNED) AND IFNULL(T110.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({ NEEDYEAR },'01') AS UNSIGNED) THEN 1
+                            ELSE 0
+                        END) = 1
+                ORDER BY T110.SORTNR;
+            """, as_dict=True)
+    
+            return data
---
+++ theodoulou/www/models/index.py
@@ -0,0 +1,17 @@
+import frappe
+from frappe import _
+from theodoulou.theodoulou.data_engine.query import TheodoulouQuery
+
+def get_context(context):
+    context.no_cache = 0
+    context.title = _('Brands')
+    context.parents = [{"name": frappe._("Home"), "route": "/"}]
+
+    query_engine = TheodoulouQuery()
+    # get type and HERNR from query string
+    type = frappe.request.args.get('type')
+    HERNR = frappe.request.args.get('HERNR')
+    NEEDYEAR = frappe.request.args.get('NEEDYEAR')    
+    # get models
+    context.models = query_engine.get_models(type, HERNR, NEEDYEAR)
+    
