c3050a910070c7854fe4295e7fbf621147ee4e4f
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-26 16:46:49 +0000

    Added fallback to retrieve vehicle_type from cookies if not present in query string

--- theodoulou/www/brands/models/index.py
+++ theodoulou/www/brands/models/index.py
@@ -10,6 +10,9 @@ def get_context(context):
     query_engine = TheodoulouQuery()
     # get type and HERNR from query string
     context.vehicle_type = frappe.request.args.get('vehicle_type')
+    # if vehicle_type is not in the query string, get it from the cookies
+    if not context.vehicle_type:
+        context.vehicle_type = frappe.request.cookies.get('vehicle_type')
     HERNR = frappe.request.args.get('HERNR')
     context.NEEDYEAR = frappe.request.args.get('NEEDYEAR')    
     # get models
