04500d0622dfea8947fc0f87cdb8c06344e29844
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-26 07:52:21 +0000

    Remove collapsible functionality from theodoulou.scss and index.html

--- theodoulou/www/vehicle/index.js
+++ theodoulou/www/vehicle/index.js
@@ -1,13 +1,3 @@
 $(document).ready(function () {
-    // Get all the collapsible headers
-    var coll = document.getElementsByClassName("collapsible-header");
-
-    // Add a click event listener to each header
-    for (var i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            // Toggle the visibility of the body
-            var body = this.nextElementSibling;
-            body.style.display = body.style.display === "none" ? "block" : "none";
-        });
-    }
+    
 });
