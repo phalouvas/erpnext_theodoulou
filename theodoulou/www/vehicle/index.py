c134b8fb128a50f1b6b5020677b4eb2a0184008d
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-26 09:06:00 +0000

    Update vehicle index.html and index.py

--- theodoulou/www/vehicle/index.py
+++ theodoulou/www/vehicle/index.py
@@ -12,16 +12,16 @@ def get_context(context):
     query_engine = TheodoulouQuery()
     # get type and KMODNR from query string
     context.type = frappe.request.args.get('type')
-    context.ID = frappe.request.args.get('ID')
+    context.vehicle_id = frappe.request.args.get('ID')
     # get vehicle
     if context.type == "PKW":
-        vehicle = query_engine.get_vehicle_passenger(context.ID)
+        vehicle = query_engine.get_vehicle_passenger(context.vehicle_id)
     else:
-        vehicle = query_engine.get_vehicle_commercial(context.ID)
+        vehicle = query_engine.get_vehicle_commercial(context.vehicle_id)
     
     context.vehicle = vehicle[0]
     context.vehicle.FROM_YEAR = query_engine.convert_yyyymm(context.vehicle.FROM_YEAR)
     context.vehicle.TO_YEAR = query_engine.convert_yyyymm(context.vehicle.TO_YEAR)
 
-    context.categories_tree = query_engine.get_vehicle_categories(type, context.ID)
+    context.categories_tree = query_engine.get_vehicle_categories(context.type, context.vehicle_id)
     
