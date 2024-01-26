d012cbae9a4201b6395639f93b8a9ebf88bfc426
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-26 07:32:40 +0000

    Add collapsible functionality to vehicle index.html and index.js

---
+++ theodoulou/www/vehicle/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // Get all the collapsible headers
+    var coll = document.getElementsByClassName("collapsible-header");
+
+    // Add a click event listener to each header
+    for (var i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            // Toggle the visibility of the body
+            var body = this.nextElementSibling;
+            body.style.display = body.style.display === "none" ? "block" : "none";
+        });
+    }
+});
