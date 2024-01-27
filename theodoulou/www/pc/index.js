21cd5e31898a302c35489631c47cbb22f3544d59
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-27 09:55:04 +0000

    Refactor search functionality and include brands.html template

--- theodoulou/www/pc/index.js
+++ theodoulou/www/pc/index.js
@@ -1,21 +1,24 @@
 $(document).ready(function () {
-    $('#searchKey').on('keyup', function () {
-        var searchKey = $(this).val().toLowerCase();
-
-        $('.searchContainer').each(function () {
-            var name = $(this).data('name');
-
-            if (name.indexOf(searchKey) !== -1) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
+    $(document).ready(function () {
+        console.log('brands.js loaded');
+        $('#searchKey').on('keyup', function () {
+            var searchKey = $(this).val().toLowerCase();
+    
+            $('.searchContainer').each(function () {
+                var name = $(this).data('name');
+    
+                if (name.indexOf(searchKey) !== -1) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
+                }
+            });
         });
+    
+        $('.clearSearch').on('click', function () {
+            $('#searchKey').val('');
+            $('.searchContainer').show();
+        }
+        );
     });
-
-    $('.clearSearch').on('click', function () {
-        $('#searchKey').val('');
-        $('.searchContainer').show();
-    }
-    );
 });
