8ff3c4fbf9c268f5bf3b3ffc55c49723690dc43b
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-27 09:35:08 +0000

    Create home page with categories Fixes #18

--- theodoulou/www/pc/index.py
+++ theodoulou/www/pc/index.py
@@ -3,6 +3,9 @@ from frappe import _
 from theodoulou.theodoulou.data_engine.query import TheodoulouQuery
 
 def get_context(context):
-    context.no_cache = 0
-    context.title = _('Brands')
+    context.no_cache = 1
+    context.title = _('Passenger Cars')
     context.parents = [{"name": frappe._("Home"), "route": "/"}]
+
+    query_engine = TheodoulouQuery()
+    context.items = query_engine.get_brands('PKW')
