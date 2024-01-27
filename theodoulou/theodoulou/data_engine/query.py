9d15ba457c3a06161b915d8eb40993e5693d65cb
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-27 05:43:15 +0000

    Refactor get_models and get_types_passenger_cars methods

--- theodoulou/theodoulou/data_engine/query.py
+++ theodoulou/theodoulou/data_engine/query.py
@@ -52,49 +52,58 @@ class TheodoulouQuery():
         return data
     
     def get_models(self, type, HERNR, NEEDYEAR = 0):
-            data = frappe.db.sql(f"""
-                SELECT
-                    T110.KMODNR,  -- ID MODEL
-                    GET_LBEZNR(T100.LBEZNR, { self.language }) AS MANUFACTURER,  -- NAME MANUFACTURER
-                    GET_LBEZNR(T110.LBEZNR, { self.language }) AS MODEL,  -- NAME MODEL
-                    T110.BJVON AS FROM_YEAR, -- MODEL PRODUCTING FROM YEAR+MONTH
-                    IFNULL(T110.BJBIS, 'now') AS TO_YEAR -- MODEL PRODUCTING TO YEAR+MONTH
-                FROM `110` AS T110
-                    JOIN `100` AS T100 ON T100.HERNR = T110.HERNR			
-                WHERE 1 
-                    AND T100.HERNR = { HERNR }
-                    AND T110.{ type } = 1
-                    AND (CASE
-                            WHEN { NEEDYEAR } = 0 OR LENGTH({ NEEDYEAR }) <> 4 THEN 1					
-                            WHEN T110.BJVON <= CAST(CONCAT({ NEEDYEAR },'01') AS UNSIGNED) AND IFNULL(T110.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({ NEEDYEAR },'01') AS UNSIGNED) THEN 1
-                            ELSE 0
-                        END) = 1
-                ORDER BY T110.SORTNR;
-            """, as_dict=True)
+            
+            data = frappe.cache().get_value('models_' + type + '_' + HERNR + '_' + NEEDYEAR)
+
+            if data is None:
+                data = frappe.db.sql(f"""
+                    SELECT
+                        T110.KMODNR,  -- ID MODEL
+                        GET_LBEZNR(T100.LBEZNR, { self.language }) AS MANUFACTURER,  -- NAME MANUFACTURER
+                        GET_LBEZNR(T110.LBEZNR, { self.language }) AS MODEL,  -- NAME MODEL
+                        T110.BJVON AS FROM_YEAR, -- MODEL PRODUCTING FROM YEAR+MONTH
+                        IFNULL(T110.BJBIS, 'now') AS TO_YEAR -- MODEL PRODUCTING TO YEAR+MONTH
+                    FROM `110` AS T110
+                        JOIN `100` AS T100 ON T100.HERNR = T110.HERNR			
+                    WHERE 1 
+                        AND T100.HERNR = { HERNR }
+                        AND T110.{ type } = 1
+                        AND (CASE
+                                WHEN { NEEDYEAR } = 0 OR LENGTH({ NEEDYEAR }) <> 4 THEN 1					
+                                WHEN T110.BJVON <= CAST(CONCAT({ NEEDYEAR },'01') AS UNSIGNED) AND IFNULL(T110.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({ NEEDYEAR },'01') AS UNSIGNED) THEN 1
+                                ELSE 0
+                            END) = 1
+                    ORDER BY T110.SORTNR;
+                """, as_dict=True)
+                frappe.cache().set_value('models_' + type + '_' + HERNR + '_' + NEEDYEAR, data)
     
             return data
     
     def get_types_passenger_cars(self, KMODNR, NEEDYEAR = 0):
-        data = frappe.db.sql(f"""
-            SELECT
-                T120.KTYPNR AS ID,  -- ID TYPE
-                GET_LBEZNR(T100.LBEZNR, { self.language }) AS MANUFACTURER,  -- NAME MANUFACTURER
-                GET_LBEZNR(T110.LBEZNR, { self.language }) AS MODEL,  -- NAME MODEL
-                GET_LBEZNR(T120.LBEZNR, { self.language }) AS TYPE,  -- NAME TYPE
-                T120.BJVON AS FROM_YEAR, -- TYPE PRODUCTING FROM YEAR+MONTH
-                IFNULL(T120.BJBIS, 'now') AS TO_YEAR -- TYPE PRODUCTING TO YEAR+MONTH								
-            FROM `120` AS T120
-                JOIN `110` AS T110 ON T110.KMODNR = T120.KMODNR			
-                JOIN `100` AS T100 ON T100.HERNR = T110.HERNR			
-            WHERE 1 
-                AND T110.KMODNR = { KMODNR }
-                AND (CASE
-                        WHEN {  NEEDYEAR } = 0 OR LENGTH({  NEEDYEAR }) <> 4 THEN 1					
-                        WHEN T120.BJVON <= CAST(CONCAT({  NEEDYEAR },'01') AS UNSIGNED) AND IFNULL(T120.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({  NEEDYEAR },'01') AS UNSIGNED) THEN 1
-                        ELSE 0
-                    END) = 1
-            ORDER BY T120.SORTNR;	
-        """, as_dict=True)
+        data = frappe.cache().get_value('types_passenger_cars_' + KMODNR + '_' + NEEDYEAR)
+
+        if data is None:
+            data = frappe.db.sql(f"""
+                SELECT
+                    T120.KTYPNR AS ID,  -- ID TYPE
+                    GET_LBEZNR(T100.LBEZNR, { self.language }) AS MANUFACTURER,  -- NAME MANUFACTURER
+                    GET_LBEZNR(T110.LBEZNR, { self.language }) AS MODEL,  -- NAME MODEL
+                    GET_LBEZNR(T120.LBEZNR, { self.language }) AS TYPE,  -- NAME TYPE
+                    T120.BJVON AS FROM_YEAR, -- TYPE PRODUCTING FROM YEAR+MONTH
+                    IFNULL(T120.BJBIS, 'now') AS TO_YEAR -- TYPE PRODUCTING TO YEAR+MONTH								
+                FROM `120` AS T120
+                    JOIN `110` AS T110 ON T110.KMODNR = T120.KMODNR			
+                    JOIN `100` AS T100 ON T100.HERNR = T110.HERNR			
+                WHERE 1 
+                    AND T110.KMODNR = { KMODNR }
+                    AND (CASE
+                            WHEN {  NEEDYEAR } = 0 OR LENGTH({  NEEDYEAR }) <> 4 THEN 1					
+                            WHEN T120.BJVON <= CAST(CONCAT({  NEEDYEAR },'01') AS UNSIGNED) AND IFNULL(T120.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({  NEEDYEAR },'01') AS UNSIGNED) THEN 1
+                            ELSE 0
+                        END) = 1
+                ORDER BY T120.SORTNR;	
+            """, as_dict=True)
+            frappe.cache().set_value('types_passenger_cars_' + KMODNR + '_' + NEEDYEAR, data)
 
         return data
     
