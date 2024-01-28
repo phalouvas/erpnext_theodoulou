278eef780af13f5c6f2720072e8a2cfc23699f0d
Panayiotis Halouvas <phalouvas@gmail.com>
2024-01-28 12:39:00 +0000

    Refactor vehicle index.html and index.js files

--- theodoulou/www/pc/models/types/vehicle/index.js
+++ theodoulou/www/pc/models/types/vehicle/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {    
+$(document).ready(function () {
     // Get the query string parameters
     var params = new URLSearchParams(window.location.search);
 
@@ -6,4 +6,13 @@ $(document).ready(function () {
     for (var pair of params.entries()) {
         document.cookie = pair[0] + "=" + pair[1];
     }
+
+    // get element data-name attribute value with id vehicle-active-selection-name and save in cookies
+    var vehicleActiveSelectionName = document.getElementById("vehicleActiveSelectionName").getAttribute("data-name");
+    document.cookie = "vehicleActiveSelectionName=" + vehicleActiveSelectionName + "; sameSite=Lax; path=/";
+
+    $('#vehicleActiveSelectionClear').on('click', function () {
+        document.cookie = 'vehicleActiveSelectionName=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        window.location.href = '/pc';
+    });
 });
