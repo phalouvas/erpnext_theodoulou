0465ca82657fbc5a4127841a5245f1b8d03cd949
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-01 12:46:53 +0000

    Add search in product details Fixes #42

--- theodoulou/www/pc/models/types/vehicle/products/product/index.js
+++ theodoulou/www/pc/models/types/vehicle/products/product/index.js
@@ -3,4 +3,18 @@ $(document).ready(function () {
         $(this).toggleClass('caret-down');
         $(this).siblings('ul').toggle();
     });
+
+    $('#searchKey_OE').on('keyup', function () {
+        var searchKey = $(this).val().toLowerCase();
+
+        $('.searchContainer_OE').each(function () {
+            var name = $(this).data('name');
+
+            if (name.indexOf(searchKey) !== -1) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
 });
