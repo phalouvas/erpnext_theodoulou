9ba851e04d4dd42dbc61ae73c090da5cef332fc4
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-02 16:00:47 +0000

    rename brands to brands_OLD

--- theodoulou/www/brands/index.js
+++ theodoulou/www/brands/index.js
@@ -1,8 +1,13 @@
-$(document).ready(function() {
-    $('#passenger_cars_searchKey').on('keyup', function() {
+$(document).ready(function () {
+    $('.tree .caret').on('click', function () {
+        $(this).toggleClass('caret-down');
+        $(this).siblings('ul').toggle();
+    });
+
+    $('#searchKey').on('keyup', function () {
         var searchKey = $(this).val().toLowerCase();
 
-        $('.passenger_cars_card_container').each(function() {
+        $('.searchContainer').each(function () {
             var name = $(this).data('name');
 
             if (name.indexOf(searchKey) !== -1) {
@@ -13,43 +18,30 @@ $(document).ready(function() {
         });
     });
 
-    $('#commercial_cars_searchKey').on('keyup', function() {
-        var searchKey = $(this).val().toLowerCase();
-
-        $('.commercial_cars_card_container').each(function() {
-            var name = $(this).data('name');
-
-            if (name.indexOf(searchKey) !== -1) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
+    $('.clearSearch').on('click', function () {
+        $('#searchKey').val('');
+        $('.searchContainer').show();
     });
 
-    // Check if the vehicle_type cookie is set
-    var vehicle_type = document.cookie.split('; ').find(row => row.startsWith('vehicle_type='));
+    $('.showAll').on('click', function () {
+        var url = window.location.href;
+        var separator = (url.indexOf('?') !== -1) ? "&" : "?";
+        window.location.href = url + separator + 'show_all=1';
+    });
 
-    // If the vehicle_type cookie is not set, set it to "PKW"
-    if (!vehicle_type) {
-        document.cookie = "vehicle_type=PKW";
-        vehicle_type = "vehicle_type=PKW";
-    }
+    $('.showPopular').on('click', function () {
+        // remove the show_all parameter
+        var url = window.location.href;
+        url = url.replace(/&?show_all=1/, '');
+        window.location.href = url;
+    });
 
-    if (vehicle_type == "vehicle_type=PKW") {
-        $('#brandsTab li:first-child a').tab('show')
+    var params = new URLSearchParams(window.location.search);
+    if (params.has('show_all')) {
+        $('.showAll').hide();
+        $('.showPopular').show();
     } else {
-        $('#brandsTab li:last-child a').tab('show')
+        $('.showAll').show();
+        $('.showPopular').hide();
     }
-
-    // If the element with ID passenger_cars-tab is clicked, set the cookie to "PKW"
-    $('#passenger_cars-tab').click(function() {
-        document.cookie = "vehicle_type=PKW";
-    });
-
-    // If the element with ID commercial_cars-tab is clicked, set the cookie to "NKW"
-    $('#commercial_cars-tab').click(function() {
-        document.cookie = "vehicle_type=NKW";
-    });
-
 });
--- theodoulou/www/brands/index.py
+++ theodoulou/www/brands/index.py
@@ -1,12 +1,18 @@
 import frappe
 from frappe import _
-from theodoulou.theodoulou.data_engine.query import TheodoulouQuery
+from theodoulou.theodoulou.data_engine.motorcycles_query import MotorcyclesQuery
 
 def get_context(context):
     context.no_cache = 0
-    context.title = _('Brands')
+    context.title = _('Motorcycles')
     context.parents = [{"name": frappe._("Home"), "route": "/"}]
 
-    query_engine = TheodoulouQuery()
-    context.passenger_cars = query_engine.get_brands('PKW')
-    context.commercial_cars = query_engine.get_brands('NKW')
+    query_engine = MotorcyclesQuery()
+    context.categories_tree = query_engine.get_categories_tree('PKW')
+
+    all_brands = query_engine.get_brands()
+    if 'show_all' in frappe.request.args:
+        context.brands = all_brands
+    else:
+        popular_brands = [4330, 2524, 16, 2559, 4055, 45, 4056, 1131, 2760, 88, 181, 109, 1021]
+        context.brands = [brand for brand in all_brands if brand['HERNR'] in popular_brands]
