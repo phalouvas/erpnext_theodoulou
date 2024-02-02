f84c10a76c278717b753c4827eead72d6e9da2b4
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-02 16:15:57 +0000

    Refactor code to use TheodoulouController for querying motorcycles

---
+++ theodoulou/theodoulou/data_engine/controller.py
@@ -0,0 +1,13 @@
+import frappe
+from theodoulou.theodoulou.data_engine.motorcycles_query import MotorcyclesQuery
+
+class TheodoulouController:
+    def __init__(self):
+        pass
+
+    def get_engine(self):
+        vehicle_type = frappe.request.args.get('vehicle_type') or 'PKW'
+        vehicle_class = int(frappe.request.args.get('vehicle_class') or 2)
+        
+        if vehicle_type == 'PKW' and vehicle_class == 6:
+            return MotorcyclesQuery()
--- theodoulou/www/brands/index.py
+++ theodoulou/www/brands/index.py
@@ -1,13 +1,14 @@
 import frappe
 from frappe import _
-from theodoulou.theodoulou.data_engine.motorcycles_query import MotorcyclesQuery
+from theodoulou.theodoulou.data_engine.controller import TheodoulouController
 
 def get_context(context):
     context.no_cache = 0
     context.title = _('Motorcycles')
     context.parents = [{"name": frappe._("Home"), "route": "/"}]
 
-    query_engine = MotorcyclesQuery()
+    query_controller = TheodoulouController()
+    query_engine = query_controller.get_engine()
     context.categories_tree = query_engine.get_categories_tree('PKW')
 
     all_brands = query_engine.get_brands()
