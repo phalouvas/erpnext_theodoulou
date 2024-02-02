99329636e2d751511eb57deda8b2af38ffdcb11b
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-02 16:56:37 +0000

    Add support for LCV queries

--- theodoulou/theodoulou/data_engine/controller.py
+++ theodoulou/theodoulou/data_engine/controller.py
@@ -1,5 +1,6 @@
 import frappe
 from theodoulou.theodoulou.data_engine.motorcycle import MotorcycleQuery
+from theodoulou.theodoulou.data_engine.lcv import LcvQuery
 
 class TheodoulouController:
     def __init__(self):
@@ -8,8 +9,8 @@ class TheodoulouController:
     def get_engine(self):
         vehicle_class = frappe.request.args.get('vehicle_class') or "PC"
 
-        if vehicle_class == 'Motorcycle':
+        if vehicle_class == 'motorcycle':
             return MotorcycleQuery()
         
-        if vehicle_class == 'E-PC':
-            return MotorcycleQuery()
+        if vehicle_class == 'lcv':
+            return LcvQuery()
---
+++ theodoulou/theodoulou/data_engine/lcv.py
@@ -0,0 +1,33 @@
+import frappe
+
+from theodoulou.theodoulou.data_engine.query import TheodoulouQuery
+
+class LcvQuery(TheodoulouQuery):
+
+    def __init__(self):
+        super().__init__()
+    
+    def get_brands(self):
+        data = frappe.cache().get_value('lcv_brands')
+        if data is None:
+            data = frappe.db.sql(f"""
+                SELECT DISTINCT 
+                    T100.HERNR,  -- ID MANUFACTURER
+                    GET_LBEZNR(T100.LBEZNR, { self.language }) AS NAME  -- NAME MANUFACTURER
+                FROM `100` AS T100
+                    JOIN `110` AS T110 ON T100.HerNr = T110.HerNr
+                    JOIN `120` AS T120 ON T110.KMODNR = T120.KMODNR
+                WHERE T110.Transporter = '1' 
+                    AND T120.MOTART <> '040'
+                ORDER BY NAME;
+            """, as_dict=True)
+            frappe.cache().set_value('lcv_brands', data)
+
+        if 'show_all' in frappe.request.args:
+            return data
+        else:
+            popular_brands = [2, 5, 16, 20, 21, 185, 24, 25, 35, 36, 45, 183,54, 882, 184, 63,
+                              72, 74, 1523, 77, 80, 84, 88, 93, 95, 104, 106, 109, 111, 120, 121, 138 ]
+            data = [brand for brand in data if brand['HERNR'] in popular_brands]
+        
+        return data
