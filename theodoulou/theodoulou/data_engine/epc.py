1bbef001d92fee1471b7fd101b036f1fc6ed7040
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-02 17:34:45 +0000

    Refactor ePcQuery get_brands method to exclude certain brands

--- theodoulou/theodoulou/data_engine/epc.py
+++ theodoulou/theodoulou/data_engine/epc.py
@@ -10,7 +10,7 @@ class ePcQuery(TheodoulouQuery):
         super().__init__()
     
     def get_brands(self):
-        data = frappe.cache().get_value('elcv_brands')
+        data = frappe.cache().get_value('epc_brands')
         if data is None:
             data = frappe.db.sql(f"""
                 SELECT DISTINCT 
@@ -23,17 +23,26 @@ class ePcQuery(TheodoulouQuery):
                     AND T120.KRSTOFFART = '11'
                 ORDER BY NAME;
             """, as_dict=True)
-            frappe.cache().set_value('elcv_brands', data)
+
+            emotorcycle_engine = eMotorcycleQuery()
+            elcv_engine = eLcvQuery()
+            emotorcycle_brands = emotorcycle_engine.get_brands()
+            elcv_brands = elcv_engine.get_brands()
+
+            # Get the HERNR values from the brands to exclude
+            exclude_brands = [brand['HERNR'] for brand in emotorcycle_brands[0] + elcv_brands[0]]
+
+            # List comprehension
+            data = [item for item in data if item['HERNR'] not in exclude_brands]
+            
+            frappe.cache().set_value('epc_brands', data)
 
         if 'show_all' in frappe.request.args:
-            pass
+            popular_data = []
         else:
-            popular_brands = [21, 35, 36, 183, 184, 74, 77, 80, 84, 88, 93, 111, 121]
-            data = [brand for brand in data if brand['HERNR'] in popular_brands]
-
-        emotorcycle_engine = eMotorcycleQuery()
-        elcv_engine = eLcvQuery()
-        emotorcycle_brands = emotorcycle_engine.get_brands()
-        elcv_brands = elcv_engine.get_brands()
+            popular_brands = [2, 5, 16, 20, 21, 24, 25, 35, 36, 45, 183, 54, 56, 
+                        882, 184, 63, 72, 74, 1523, 77, 80, 84, 88, 92, 93, 95, 1820,
+                        99, 104, 106, 1138, 107, 109, 111, 120, 121]
+            popular_data = [brand for brand in data if brand['HERNR'] in popular_brands]
         
-        return data
+        return data, popular_data
