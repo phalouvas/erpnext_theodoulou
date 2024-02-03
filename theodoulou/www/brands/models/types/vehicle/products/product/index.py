7add8cde61d97128b9a5bd53a91eddb7f174b467
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-03 16:45:22 +0000

    Refactor TheodoulouQuery to TheodoulouController in index.py

--- theodoulou/www/brands/models/types/vehicle/products/product/index.py
+++ theodoulou/www/brands/models/types/vehicle/products/product/index.py
@@ -1,6 +1,6 @@
 import frappe
 from frappe import _
-from theodoulou.theodoulou.data_engine.query import TheodoulouQuery
+from theodoulou.theodoulou.data_engine.controller import TheodoulouController
 
 def get_context(context):
 
@@ -10,31 +10,36 @@ def get_context(context):
     # If the cookie is empty, throw an exception
     if not vehicleActiveSelectionName:
         frappe.throw(_("Please select a vehicle first."))
+
+    query_controller = TheodoulouController()
+    query_engine = query_controller.get_engine()
     
-    vehicle_id = frappe.request.args.get('vehicle_id') or frappe.request.cookies.get('vehicle_id')
-    brand_id = frappe.request.args.get('brand_id') or frappe.request.cookies.get('brand_id')
-    needyear = frappe.request.args.get('needyear') or frappe.request.cookies.get('needyear')
-    model_id = frappe.request.args.get('model_id') or frappe.request.cookies.get('model_id')
+    context.BrandClass = query_controller.BrandClass
+    context.ManNo = frappe.request.args.get('ManNo') or frappe.request.cookies.get('ManNo')
+    context.KModNo = frappe.request.args.get('KModNo') or frappe.request.cookies.get('KModNo')
+    context.needyear = frappe.request.args.get('needyear') or frappe.request.cookies.get('needyear')
+    context.KTypNo = frappe.request.args.get('KTypNo') or frappe.request.cookies.get('KTypNo')
     context.node_id = frappe.request.args.get('node_id') or frappe.request.cookies.get('node_id')
-    product_id = frappe.request.args.get('product_id')
+    ArtNo = frappe.request.args.get('ArtNo')
 
-    query_engine = TheodoulouQuery()
-    manufacturer_id = frappe.request.args.get('manufacturer_id') or query_engine.dlnr_from_artnr(product_id)
-    context.product_main_info = query_engine.get_product_main_info(manufacturer_id, product_id)
-    context.product_criteria = query_engine.get_product_criteria(manufacturer_id, product_id)
-    context.product_additional_info = query_engine.get_product_additional_info(manufacturer_id, product_id)
-    context.product_oe_numbers = query_engine.get_product_oe_numbers(manufacturer_id, product_id)
-    context.product_vehicles_applicability = query_engine.get_product_vehicles_applicability(manufacturer_id, product_id)
-    context.product_media = query_engine.get_product_media(manufacturer_id, product_id)
-    context.manufacturer_logo = query_engine.get_manufacturer_logo(manufacturer_id)
-    context.product_analogs = query_engine.get_product_analogs(product_id)
+    BrandNo = frappe.request.args.get('BrandNo') or query_engine.dlnr_from_artnr(ArtNo)
+    context.product_main_info = query_engine.get_product_main_info(BrandNo, ArtNo)
+    context.product_criteria = query_engine.get_product_criteria(BrandNo, ArtNo)
+    context.product_additional_info = query_engine.get_product_additional_info(BrandNo, ArtNo)
+    context.product_oe_numbers = query_engine.get_product_oe_numbers(BrandNo, ArtNo)
+    context.product_vehicles_applicability = query_engine.get_product_vehicles_applicability(BrandNo, ArtNo)
+    context.product_media = query_engine.get_product_media(BrandNo, ArtNo)
+    context.manufacturer_logo = query_engine.get_manufacturer_logo(BrandNo)
+    context.product_analogs = query_engine.get_product_analogs(ArtNo)
 
-    context.categories_tree = query_engine.get_categories_tree("PKW")
+    context.categories_tree = query_engine.get_categories_tree()
 
     context.no_cache = 0
     context.title = f"{context.product_main_info.MARKE} - {context.product_main_info.ARTNR}"
     context.parents = [
-        {"name": frappe._("Home"), "route": "/"}, 
-        {"name": frappe._("Passenger Cars"), "route": "/pc"}, 
-        {"name": frappe._("Vehicle"), "route": f"/pc/models/types/vehicle?brand_id={brand_id}&model_id={model_id}&needyear={needyear}&vehicle_id={vehicle_id}"}, 
+        {"name": _("Home"), "route": "/"}, 
+        {"name": query_engine.title, "route": f"/brands?BrandClass={query_controller.BrandClass}"},
+        {"name": _("Models"), "route": f"/brands/models?BrandClass={query_controller.BrandClass}&ManNo={context.ManNo}&needyear={context.needyear}"}, 
+        {"name": _("Types"), "route": f"/brands/models/types?BrandClass={query_controller.BrandClass}&ManNo={context.ManNo}&KModNo={context.KModNo}&needyear={context.needyear}"}, 
+        {"name": _("Vehicle"), "route": f"/brands/models/types/vehicle?BrandClass={query_controller.BrandClass}&ManNo={context.ManNo}&KModNo={context.KModNo}&KTypNo={context.KTypNo}&needyear={context.needyear}"},
     ]
