807b7f6d85deb96b9b90a11d42f2def81a421e98
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-03 13:54:37 +0000

    Add get_models method to CvQuery, BusQuery, and LcvQuery

--- theodoulou/theodoulou/data_engine/epc.py
+++ theodoulou/theodoulou/data_engine/epc.py
@@ -27,3 +27,36 @@ class ePcQuery(TheodoulouQuery):
             frappe.cache().set_value('epc_brands', data)
 
         return data
+    
+    def get_models(self, ManNo, NeedYear = 0):
+            
+            data = frappe.cache().get_value('emotorcycles_brands' + '_' + ManNo + '_' + NeedYear)
+            data= None
+
+            if data is None:
+                data = frappe.db.sql(f"""
+                    SELECT
+                        T110.KMODNR AS KModNo,  -- ID MODEL
+                        GET_LBEZNR(T100.LBEZNR, { self.language }) AS MANUFACTURER,  -- NAME MANUFACTURER
+                        GET_LBEZNR(T110.LBEZNR, { self.language }) AS MODEL,  -- NAME MODEL
+                        T110.BJVON AS FROM_YEAR, -- MODEL PRODUCTING FROM YEAR+MONTH
+                        IFNULL(T110.BJBIS, 'now') AS TO_YEAR -- MODEL PRODUCTING TO YEAR+MONTH
+                    FROM `110` AS T110
+                        JOIN `100` AS T100 ON T100.HerNr = T110.HerNr
+                        JOIN `120` AS T120 ON T110.KMODNR = T120.KMODNR		
+                    WHERE 1 
+                        AND T120.MOTART = '040'
+                        AND T120.KRSTOFFART = '11'
+                        AND T120.AUFBAUART <> '051'
+                        AND T110.Transporter = '0'
+                        AND T100.HERNR = { ManNo }
+                        AND (CASE
+                                WHEN { NeedYear } = 0 OR LENGTH({ NeedYear }) <> 4 THEN 1					
+                                WHEN T110.BJVON <= CAST(CONCAT({ NeedYear },'01') AS UNSIGNED) AND IFNULL(T110.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({ NeedYear },'01') AS UNSIGNED) THEN 1
+                                ELSE 0
+                            END) = 1
+                    ORDER BY T110.SORTNR;
+                """, as_dict=True)
+                frappe.cache().set_value('emotorcycles_brands' + '_' + ManNo + '_' + NeedYear, data)
+    
+            return data
