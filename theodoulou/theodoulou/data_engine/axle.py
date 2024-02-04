fb34a02e05c0d88da1094f8d8c61174478a3cb8b
Panayiotis Halouvas <phalouvas@gmail.com>
2024-02-04 07:06:19 +0000

    Refactor AxleQuery SQL query in axle.py

--- theodoulou/theodoulou/data_engine/axle.py
+++ theodoulou/theodoulou/data_engine/axle.py
@@ -56,25 +56,25 @@ class AxleQuery(TheodoulouQuery):
 
         if data is None:
             data = frappe.db.sql(f"""
-                SELECT DISTINCT
-                    T120.KTYPNR AS KTypNo,  -- ID TYPE
+                SELECT
+                    T160.ATYPNR AS KTypNo,  -- ID TYPE
                     GET_LBEZNR(T100.LBEZNR, { self.language }) AS MANUFACTURER,  -- NAME MANUFACTURER
                     GET_LBEZNR(T110.LBEZNR, { self.language }) AS MODEL,  -- NAME MODEL
-                    GET_LBEZNR(T120.LBEZNR, { self.language }) AS TYPE,  -- NAME TYPE
-                    T120.BJVON AS FROM_YEAR, -- TYPE PRODUCTING FROM YEAR+MONTH
-                    IFNULL(T120.BJBIS, 'now') AS TO_YEAR -- TYPE PRODUCTING TO YEAR+MONTH								
-                FROM `120` AS T120
-                    JOIN `110` AS T110 ON T110.KMODNR = T120.KMODNR			
+                    T160.BEZEICHNUNG AS TYPE,  -- NAME TYPE
+                    T160.BJVON AS FROM_YEAR, -- TYPE PRODUCTING FROM YEAR+MONTH
+                    IFNULL(T160.BJBIS, 'now') AS TO_YEAR -- TYPE PRODUCTING TO YEAR+MONTH								
+                FROM `160` AS T160
+                    JOIN `110` AS T110 ON T110.KMODNR = T160.KMODNR			
                     JOIN `100` AS T100 ON T100.HERNR = T110.HERNR			
                 WHERE 1 
-                    AND T110.KMODNR = { KMODNR }
+                    AND T110.KMODNR = {KModNo}
                     AND (CASE
-                            WHEN {  NEEDYEAR } = 0 OR LENGTH({  NEEDYEAR }) <> 4 THEN 1					
-                            WHEN T120.BJVON <= CAST(CONCAT({  NEEDYEAR },'01') AS UNSIGNED) AND IFNULL(T120.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({  NEEDYEAR },'01') AS UNSIGNED) THEN 1
+                            WHEN {needyear} = 0 OR LENGTH({needyear}) <> 4 THEN 1					
+                            WHEN T160.BJVON <= CAST(CONCAT({needyear},'01') AS UNSIGNED) AND IFNULL(T160.BJBIS, CAST(CONCAT(YEAR(NOW()),'12') AS UNSIGNED)) >= CAST(CONCAT({needyear},'01') AS UNSIGNED) THEN 1
                             ELSE 0
                         END) = 1
-                ORDER BY T120.SORTNR;	
+                ORDER BY T160.SORTNR;	
             """, as_dict=True)
-            frappe.cache().set_value(f"types_{BrandClass}_{KModNo}_{needyear}")
+            frappe.cache().set_value(f"types_{BrandClass}_{KModNo}_{needyear}", data)
 
         return data
